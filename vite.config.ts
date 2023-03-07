5bfae8af63e909ae1509d9e493ec1beb4ce770b1
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-03-06 23:05:14 -0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["icons/*.{png,ico,svg}"],
+      manifest: {
+        name: "Vite PWA",
+        short_name: "VitePWA",
+        description: "PWA test with Vite.js",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
